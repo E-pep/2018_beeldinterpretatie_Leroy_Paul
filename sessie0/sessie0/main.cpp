346defc3f6e3a123c6c8a7ae346b35213d6ead97
E-pep <paul.leroy1996@hotmail.com>
2018-10-22 19:19:30 +0200

    show window not working

--- sessie0/sessie0/main.cpp
+++ sessie0/sessie0/main.cpp
@@ -6,7 +6,12 @@ using namespace cv;
 
 int main(int argc,const char** argv)
 {
-    cout << "Hello world!" << endl;
+    ///variables
+    string ImageName1;
+    string ImageName2;
+    Mat Image1;
+    Mat Image2;
+
 
     ///Adding a little help option and command line parser input
     CommandLineParser parser(argc,argv,
@@ -17,31 +22,49 @@ int main(int argc,const char** argv)
 
     if(parser.has("help"))
     {
+        cout << "geef volledig path van foto's mee als argument" << endl;
         parser.printMessage();
         return 0;
     }
 
+    if( argc <= 2)
+    {
+        cout << "te weinig argumenten meegegeven" << endl;
+        return -1;
+    }
+
+
     ///Collect data from arguments
-    string image1(parser.get<string>("image_gray"));
-    if(image1.empty())
+
+    ImageName1 = argv[1];
+    ImageName2 = argv[2];
+
+    cout << ImageName1 << endl;
+    cout << ImageName2 << endl;
+
+    ///inlezen van afbeelding1
+
+   Image1 = imread("/home/paul/Desktop/school/1819/2018_labo_beeldinterpretatie/introductie/test.png", IMREAD_COLOR);
+
+   cout << Image1 << endl;
+
+   Image2 = imread("/home/paul/Desktop/school/1819/2018_labo_beeldinterpretatie/introductie/testColor.png", IMREAD_COLOR);
+
+    if( Image1.empty() )                      /// Check for invalid input
     {
-        cout << "image1 niet meegegeven" << endl;
-        parser.printMessage();
+        cout <<  "Could not open or find image1" << std::endl ;
         return -1;
     }
 
-    string image2(parser.get<string>("image_color"));
-    if(image2.empty())
+    if( Image2.empty() )                      /// Check for invalid input
     {
-        cout << "image 2 niet meegegeven" << endl;
-        parser.printMessage();
+        cout <<  "Could not open or find image2" << std::endl ;
         return -1;
     }
 
-    ///inlezen van afbeelding
-   /// Mat img = imgread(parameter);
-
-
+    namedWindow( "Display window", WINDOW_AUTOSIZE ); /// Create a window for display.
+    imshow( "Display window", Image1 );                /// Show our image inside it.
+    waitKey(0);                                     /// Wait for a keystroke in the window
 
     return 0;
 }
