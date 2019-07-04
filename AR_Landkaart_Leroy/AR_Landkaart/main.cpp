cda57fd6ef9cfa92292030b8f5c5287d1bc55d5a
E-pep <paul.leroy1996@hotmail.com>
2019-07-04 21:55:15 +0200

    problem with contours of map

--- AR_Landkaart_Leroy/AR_Landkaart/main.cpp
+++ AR_Landkaart_Leroy/AR_Landkaart/main.cpp
@@ -8,7 +8,9 @@ using namespace cv;
 ///--------------------------------------Variabelen-------------------------------------
 Mat testafbeelding;
 Mat testafbeelding_gray;
-Mat dst, detected_edges;
+Mat cannyafbeelding;
+Mat mapafbeelding;
+Mat mapafbeelding_gray;
 vector<vector<Point> > contours;
 vector<Vec4i> hierarchy;
 RNG rng(12345);
@@ -28,6 +30,7 @@ int main(int argc,const char** argv)
     CommandLineParser parser(argc,argv,
     "{help h|  |show this message}"
     "{image  im|  |(required)}"
+    "{map  mp|  |(required)}"
     );
 
     if(parser.has("help"))
@@ -54,26 +57,77 @@ int main(int argc,const char** argv)
         return -1;
     }
 
+
+
+    string map_string(parser.get<string>("map"));
+    if(map_string.empty())
+    {
+        cout << "argument niet gevonden" << endl;
+        parser.printMessage();
+        return -1;
+    }
+
+    mapafbeelding = imread(map_string, IMREAD_COLOR);
+
+    if( mapafbeelding.empty() )                      /// Check for invalid input
+    {
+        cout <<  "Could not open or find image1" << std::endl ;
+        return -1;
+    }
+
     cvtColor( testafbeelding, testafbeelding_gray, COLOR_BGR2GRAY );
-
-    Mat canny_output;
-    Canny( testafbeelding_gray, dst, 100, 100*2 );
+    cvtColor( mapafbeelding, mapafbeelding_gray, COLOR_BGR2GRAY );
 
 
-    findContours(dst, contours,hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE);
+    Canny( testafbeelding_gray, cannyafbeelding, 100, 100*2 );
+    imshow( "canny afbeelding", cannyafbeelding );
+
+    findContours(cannyafbeelding, contours,hierarchy, RETR_EXTERNAL, CHAIN_APPROX_NONE);
 
 
   /// Draw contours
-  Mat drawing = Mat::zeros( dst.size(), CV_8UC3 );
-  for( int i = 0; i< contours.size(); i++ )
-     {
-       Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
-       drawContours( drawing, contours, i, color, 2, 8, hierarchy, 0, Point() );
-     }
+  Mat masker = Mat::zeros(cannyafbeelding.rows, cannyafbeelding.cols, CV_8UC1);
 
+  drawContours(masker, contours, -1, Scalar(255), CV_FILLED);
   /// Show in a window
   namedWindow( "Contours", CV_WINDOW_AUTOSIZE );
-  imshow( "Contours", drawing );
+  imshow( "Contours", masker );
+
+
+  // HU moments
+
+  // Calculate Moments
+  double d1 = matchShapes(masker, masker, CONTOURS_MATCH_I1, 0);
+
+
+  ///OTSU threshold voor map beter
+
+   Mat mapafbeelding_otsu;
+   threshold(mapafbeelding_gray,mapafbeelding_otsu,0,255,THRESH_OTSU | THRESH_BINARY_INV);
+   imshow("OTSU",mapafbeelding_otsu);
+
+   erode(mapafbeelding_otsu, mapafbeelding_otsu, Mat(), Point(-1,-1), 2);
+   //dilate(mapafbeelding_otsu, mapafbeelding_otsu, Mat(), Point(-1,-1), 2);
+imshow("opening", mapafbeelding_otsu);
+
+    //vinden van connected components (landen)
+    vector<vector<Point> > contours_map;
+    findContours(mapafbeelding_otsu, contours_map,hierarchy, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+
+
+        vector< vector<Point>> hulls;
+
+        for(size_t i=0; i<contours_map.size(); i++)
+        {
+        vector<Point> hull;
+        convexHull(contours_map[i], hull);
+        hulls.push_back(hull);
+        }
+     Mat mask3 = Mat::zeros(mapafbeelding_otsu.rows, mapafbeelding_otsu.cols, CV_8UC1);
+    drawContours(mask3, hulls, -1, 255, -1);
+    imshow("contours_map", mask3);
+
 
     waitKey(0);
     return 0;
